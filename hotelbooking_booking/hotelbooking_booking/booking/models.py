afaed8be8cce2a68fb32978e452311ed4acc0c32
Yonas Adiel <yonazadielwiguna@gmail.com>
2018-11-12 12:52:11 +0700

    booking done

--- hotelbooking_booking/hotelbooking_booking/booking/models.py
+++ hotelbooking_booking/hotelbooking_booking/booking/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import requests
 
 
 class Booking(models.Model):
@@ -8,5 +9,17 @@ class Booking(models.Model):
     check_out = models.DateField()
     payment_name = models.CharField(max_length=100, null=True, blank=True)
     payment_type = models.CharField(max_length=20, null=True, blank=True)
-    price = models.DecimalField(max_digits=14, decimal_places=2)
     room_number = models.CharField(max_length=3, null=True)
+    room_type = models.IntegerField(null=True)
+    price = models.DecimalField(max_digits=14, decimal_places=2)
+    guest_id = models.IntegerField(null=True)
+
+    def update_room(self):
+        r = requests.get('http://localhost:8004/api/room/type/%d/' % (self.room_type))
+        res = r.json()
+        self.room_number = res[0].get('room_number')
+        self.price = res[0].get('type_room').get('price')
+
+    def save(self):
+        self.update_room()
+        super(Booking, self).save()
---
+++ hotelbooking_booking/hotelbooking_booking/booking/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from hotelbooking_booking.booking.models import Booking
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = (
+            'id', 'check_in', 'check_out', 'payment_name', 'payment_type',
+            'room_type', 'guest_id', 'room_number', 'price')
+        read_only_fields = ('id', 'room_number', 'price')
